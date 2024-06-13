6893a1a4bdbef6b21fd0a93be2fc10ee5e306587
Max Kakunia <maksimkharkov54@gmail.com>
2024-06-13 15:27:58 -0700

    Improved user data model implemented in the database

--- IdentityCore.DAL.Models/Base/BaseDbEntity.cs
+++ IdentityCore.DAL.Models/Base/BaseDbEntity.cs
@@ -1,7 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace IdentityCore.DAL.Models.Base;
 
 public abstract record BaseDbEntity
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    public Guid Id { get; init; } = Guid.NewGuid();
+    
     public DateTime Created { get; set; } = DateTime.UtcNow;
     public DateTime Modified { get; set; } = DateTime.UtcNow;
 }
