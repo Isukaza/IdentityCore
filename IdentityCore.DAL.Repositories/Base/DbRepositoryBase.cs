a155df1a69d23cc394aeffd8537e7d406ef65472
Max Kakunia <maksimkharkov54@gmail.com>
2024-06-13 15:36:35 -0700

    Improved base repository. Added CRUD

--- IdentityCore.DAL.Repositories/Base/DbRepositoryBase.cs
+++ IdentityCore.DAL.Repositories/Base/DbRepositoryBase.cs
@@ -1,5 +1,3 @@
-using Microsoft.Extensions.Logging;
-
 using IdentityCore.DAL.MariaDb;
 using IdentityCore.DAL.Models.Base;
 
@@ -10,16 +8,59 @@ public abstract class DbRepositoryBase<T>
 {
     #region Fields and c-tor
 
-    protected readonly ILogger<DbRepositoryBase<T>> Logger;
     protected readonly IdentityCoreDbContext DbContext;
 
-    protected DbRepositoryBase(
-        IdentityCoreDbContext dbContext,
-        ILogger<DbRepositoryBase<T>> logger)
+    protected DbRepositoryBase(IdentityCoreDbContext dbContext)
     {
         DbContext = dbContext;
-        Logger = logger;
     }
+
+    #endregion
+    
+    #region CRUD
+
+    public virtual T Create(T entity)
+    {
+        var entityEntry = DbContext.Add(entity);
+        return DbContext.SaveAndCompareAffectedRows()
+            ? entityEntry.Entity
+            : default;
+    }
+
+    public virtual bool Update(T entity)
+    {
+        DbContext.Update(entity);
+        return DbContext.SaveAndCompareAffectedRows();
+    }
+
+    public virtual bool Delete(T entity)
+    {
+        DbContext.Remove(entity);
+        return DbContext.SaveAndCompareAffectedRows();
+    }
+
+    public virtual async Task<T> CreateAsync(T entity)
+    {
+        var entityEntry = await DbContext.AddAsync(entity);
+        return await DbContext.SaveAndCompareAffectedRowsAsync()
+            ? entityEntry.Entity
+            : null;
+    }
+
+    public virtual async Task<bool> UpdateAsync(T entity)
+    {
+        DbContext.Update(entity);
+        return await DbContext.SaveAndCompareAffectedRowsAsync();
+    }
+
+    public virtual async Task<bool> DeleteAsync(T entity)
+    {
+        DbContext.Remove(entity);
+        return await DbContext.SaveAndCompareAffectedRowsAsync();
+    }
+
+    public async Task<bool> SaveAsync() =>
+        await DbContext.SaveAndCompareAffectedRowsAsync();
 
     #endregion
 }
