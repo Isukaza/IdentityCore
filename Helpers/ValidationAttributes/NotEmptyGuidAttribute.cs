4104e51e7b83237049768cd9a34ed8ed1650de05
Max Kakunia <maksimkharkov54@gmail.com>
2024-06-17 14:51:35 -0700

    Added validator for empty GUID

---
+++ Helpers/ValidationAttributes/NotEmptyGuidAttribute.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Helpers.ValidationAttributes;
+
+public class NotEmptyGuidAttribute(string errorMessage) : ValidationAttribute
+{
+    private string ErrorMessage { get; set; } = errorMessage;
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if (value is Guid guidValue && guidValue == Guid.Empty)
+        {
+            return new ValidationResult(ErrorMessage);
+        }
+        return ValidationResult.Success;
+    }
+}
