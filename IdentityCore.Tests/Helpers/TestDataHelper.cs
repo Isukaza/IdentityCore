9e49eab898adee3fe49c2e119dceba832ca38fa2
Max Kakunia <maksimkharkov54@gmail.com>
2024-09-03 05:59:37 -0700

    Added random token generation

--- IdentityCore.Tests/Helpers/TestDataHelper.cs
+++ IdentityCore.Tests/Helpers/TestDataHelper.cs
@@ -20,20 +20,26 @@ public static class TestDataHelper
         return new string(randomBytes.Select(b => Chars[b % Chars.Length]).ToArray());
     }
     
-    public static string GenerateSalt()
+    public static string GetPasswordHash(string password, string salt)
+    {
+        var data = Encoding.UTF8.GetBytes(string.Concat(password, salt));
+        var hashBytes = SHA512.HashData(data);
+        return Convert.ToBase64String(hashBytes);
+    }
+    
+    public static string GenerateSalt() =>
+        GenerateRandomString();
+    
+    public static string GenerateRandomToken() =>
+        GenerateRandomString();
+
+    private static string GenerateRandomString(int length=64)
     {
         var randomBytes = new byte[64];
         using (var rng = RandomNumberGenerator.Create())
             rng.GetBytes(randomBytes);
         
         var hashBytes = SHA512.HashData(randomBytes);
-        return Convert.ToBase64String(hashBytes);
-    }
-
-    public static string GetPasswordHash(string password, string salt)
-    {
-        var data = Encoding.UTF8.GetBytes(string.Concat(password, salt));
-        var hashBytes = SHA512.HashData(data);
         return Convert.ToBase64String(hashBytes);
     }
 }
