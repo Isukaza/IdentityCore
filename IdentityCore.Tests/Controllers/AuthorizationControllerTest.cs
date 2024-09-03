868f4f0fbe070562e3a76781362cb2771f53ecde
Max Kakunia <maksimkharkov54@gmail.com>
2024-09-03 06:06:47 -0700

    Added unit tests for AuthorizationController

--- IdentityCore.Tests/Controllers/AuthorizationControllerTest.cs
+++ IdentityCore.Tests/Controllers/AuthorizationControllerTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -11,6 +12,7 @@ using IdentityCore.Controllers;
 using IdentityCore.DAL.Models;
 using IdentityCore.DAL.PorstgreSQL;
 using IdentityCore.Models.Request;
+
 using Moq;
 using NUnit.Framework;
 
@@ -30,7 +32,7 @@ public class AuthorizationControllerTest
     private readonly string _defaultPassword = Helpers.TestDataHelper.GeneratePassword();
 
     private List<User> _users;
-    
+
     #endregion
 
     [OneTimeSetUp]
@@ -39,7 +41,7 @@ public class AuthorizationControllerTest
         _httpContextMock = Mocks.MockFactory.GetHttpContextMock();
         _dbContext = _httpContextMock.Object.RequestServices.GetService<IdentityCoreDbContext>();
         _controller = Mocks.MockFactory.GetController<AuthorizationController>(_httpContextMock.Object);
-        _users = Helpers.TestDbEntityBuilder.GenerateUsers(_defaultPassword, 10);
+        _users = Helpers.TestDbEntityBuilder.GenerateUsers(_defaultPassword, 10, 2);
 
         await _dbContext.Database.EnsureCreatedAsync();
         await _dbContext.Users.AddRangeAsync(_users);
@@ -51,18 +53,18 @@ public class AuthorizationControllerTest
     {
         // Arrange
         var user = _users[_random.Next(_users.Count)];
-        var authData = new UserLoginRequest()
+        var authData = new UserLoginRequest
         {
             Email = user.Email,
             Password = _defaultPassword
         };
-        
+
         // Act
         var response = await _controller.Login(authData) as ObjectResult;
-        
+
         // Assert
         Assert.That(response, Is.Not.Null);
-        Assert.That(StatusCodes.Status200OK, Is.EqualTo(response.StatusCode));
+        Assert.That(response.StatusCode, Is.EqualTo(StatusCodes.Status200OK));
     }
 
     [Test]
@@ -81,9 +83,9 @@ public class AuthorizationControllerTest
 
         // Assert
         Assert.That(response, Is.Not.Null);
-        Assert.That(StatusCodes.Status400BadRequest, Is.EqualTo(response.StatusCode));
+        Assert.That(response.StatusCode, Is.EqualTo(StatusCodes.Status400BadRequest));
     }
-    
+
     [Test]
     public async Task Login_InvalidEmail_ReturnsBadRequest()
     {
@@ -99,7 +101,103 @@ public class AuthorizationControllerTest
 
         // Assert
         Assert.That(response, Is.Not.Null);
-        Assert.That(StatusCodes.Status400BadRequest, Is.EqualTo(response.StatusCode));
+        Assert.That(response.StatusCode, Is.EqualTo(StatusCodes.Status400BadRequest));
+    }
+
+    [Test]
+    public async Task Refresh_ValidCredentials_ReturnsOk()
+    {
+        // Arrange
+        var user = _users[_random.Next(_users.Count)];
+        Assert.That(0, Is.Not.EqualTo(user.RefreshTokens.Count));
+
+        var refreshToken = user.RefreshTokens.FirstOrDefault();
+        Assert.That(refreshToken, Is.Not.Null);
+
+        var refreshTokenRequest = new RefreshTokenRequest
+        {
+            UserId = refreshToken.UserId,
+            RefreshToken = refreshToken.RefToken
+        };
+
+        // Act
+        var response = await _controller.Refresh(refreshTokenRequest) as ObjectResult;
+
+        // Assert
+        Assert.That(response, Is.Not.Null);
+        Assert.That(response.StatusCode, Is.EqualTo(StatusCodes.Status200OK));
+    }
+
+    [Test]
+    public async Task Refresh_InvalidTokenValue_ReturnsBadRequest()
+    {
+        // Arrange
+        var user = _users[_random.Next(_users.Count)];
+        Assert.That(0, Is.Not.EqualTo(user.RefreshTokens.Count));
+
+        var refreshToken = user.RefreshTokens.FirstOrDefault();
+        Assert.That(refreshToken, Is.Not.Null);
+
+        var refreshTokenRequest = new RefreshTokenRequest
+        {
+            UserId = refreshToken.UserId,
+            RefreshToken = "InvalidRefreshToken"
+        };
+
+        // Act
+        var response = await _controller.Refresh(refreshTokenRequest) as ObjectResult;
+
+        // Assert
+        Assert.That(response, Is.Not.Null);
+        Assert.That(response.StatusCode, Is.EqualTo(StatusCodes.Status400BadRequest));
+    }
+
+    [Test]
+    public async Task Logout_ValidCredentials_ReturnsOk()
+    {
+        // Arrange
+        var user = _users[_random.Next(_users.Count)];
+        Assert.That(0, Is.Not.EqualTo(user.RefreshTokens.Count));
+
+        var refreshToken = user.RefreshTokens.FirstOrDefault();
+        Assert.That(refreshToken, Is.Not.Null);
+
+        var logoutData = new LogoutRequest
+        {
+            UserId = refreshToken.UserId,
+            RefreshToken = refreshToken.RefToken
+        };
+
+        // Act
+        var response = await _controller.Logout(logoutData) as ObjectResult;
+
+        // Assert
+        Assert.That(response, Is.Not.Null);
+        Assert.That(response.StatusCode, Is.EqualTo(StatusCodes.Status200OK));
+    }
+
+    [Test]
+    public async Task Logout_InvalidTokenValue_ReturnsOk()
+    {
+        // Arrange
+        var user = _users[_random.Next(_users.Count)];
+        Assert.That(0, Is.Not.EqualTo(user.RefreshTokens.Count));
+
+        var refreshToken = user.RefreshTokens.FirstOrDefault();
+        Assert.That(refreshToken, Is.Not.Null);
+
+        var logoutData = new LogoutRequest
+        {
+            UserId = refreshToken.UserId,
+            RefreshToken = refreshToken.RefToken
+        };
+
+        // Act
+        var response = await _controller.Logout(logoutData) as ObjectResult;
+
+        // Assert
+        Assert.That(response, Is.Not.Null);
+        Assert.That(response.StatusCode, Is.EqualTo(StatusCodes.Status200OK));
     }
 
     [OneTimeTearDown]
