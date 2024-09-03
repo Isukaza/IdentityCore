fa0a2edba095818f5954760a75d72ef3eac7b653
Max Kakunia <maksimkharkov54@gmail.com>
2024-09-03 06:03:06 -0700

    Improved generate User, added generate refresh token

--- IdentityCore.Tests/Helpers/TestDbEntityBuilder.cs
+++ IdentityCore.Tests/Helpers/TestDbEntityBuilder.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using IdentityCore.DAL.Models;
 
@@ -5,30 +6,49 @@ namespace IdentityCore.Tests.Helpers;
 
 public static class TestDbEntityBuilder
 {
-    public static List<User> GenerateUsers(string password, int count = 1, List<RefreshToken> refreshTokens = null)
+    public static List<User> GenerateUsers(string password, int userCount = 1, int tokensPerUser = 0, int tokenDurationDays = 1)
     {
         var users = new List<User>();
-
-        for (var i = 0; i < count; i++)
+        for (var i = 0; i < userCount; i++)
         {
             var username = $"user{i + 1}";
-            var email = $"{username}@example.com";
             var salt = TestDataHelper.GenerateSalt();
             var user = new User
             {
                 Username = username,
-                Email = email,
+                Email = $"{username}@example.com",
                 Password = TestDataHelper.GetPasswordHash(password, salt),
                 Salt = salt,
                 IsActive = true
             };
 
-            if (refreshTokens != null)
-                user.RefreshTokens = refreshTokens;
+            if (tokensPerUser > 0)
+            {
+                var tokens = GenerateRefreshTokens(tokensPerUser, tokenDurationDays, user.Id);
+                user.RefreshTokens = new HashSet<RefreshToken>(tokens);
+            }
 
             users.Add(user);
         }
 
         return users;
+    }
+
+    private static List<RefreshToken> GenerateRefreshTokens(int count = 1, int durationDays = 1, Guid userId = default)
+    {
+        var tokens = new List<RefreshToken>();
+        for (var i = 0; i < count; i++)
+        {
+            var token = new RefreshToken
+            {
+                RefToken = Guid.NewGuid().ToString(),
+                Expires = DateTime.UtcNow.AddDays(durationDays),
+                UserId = userId
+            };
+
+            tokens.Add(token);
+        }
+
+        return tokens;
     }
 }
