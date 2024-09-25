589a579f5bb0251080a330ff68715f41c913dca2
Max Kakunia <maksimkharkov54@gmail.com>
2024-09-25 02:17:15 -0700

    Removed obsolete entities

--- IdentityCore/Models/Request/CfmTokenRequest.cs
+++ IdentityCore/Models/Request/CfmTokenRequest.cs
@@ -1,6 +1,15 @@
-using IdentityCore.Models.Base;
+using System.ComponentModel.DataAnnotations;
+using Helpers.ValidationAttributes;
+using IdentityCore.DAL.PostgreSQL.Models.enums;
 
 namespace IdentityCore.Models.Request;
 
-public record CfmTokenRequest : ConfirmTokenBase
-{ }
+public record CfmTokenRequest
+{
+    [Required]
+    [ValidToken]
+    public string Token { get; init; }
+
+    [Required]
+    public TokenType TokenType { get; init; }
+}
